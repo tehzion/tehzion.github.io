909607a177e4044b6e84bb8e56c1b4f69dc7cc8e
tehzion <tehyongli97@gmail.com>
2020-11-11 00:31:57 +0800

    admin login update

--- yltablebooking/scripts/index.js
+++ yltablebooking/scripts/index.js
@@ -59,7 +59,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "admin" && password == "admin123"){
 alert ("Login successfully");
-window.location = "success.html"; // Redirecting to other page.
+window.location = "https://tehzion.github.io/yltablebooking/admin.html"; // Redirecting to other page.
 return false;
 }
 else{
